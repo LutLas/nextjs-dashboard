6ec87876aace985b8ebd077c866a56c02b0d77b0
LutLas <lutakachihota@gmail.com>
2025-10-02 10:34:09 +0200

    vercel team lessons
    just going through vercel teams lessons

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '172.23.128.1'],
 };
 
 export default nextConfig;
